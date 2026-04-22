ba42a310357c1cc96f21aea3af56fcb6a3d662d0
EuphoriaSYX <loskidsllorones@gmail.com>
2026-04-21 20:55:37 -0500

    versión 2.0.0

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola, tu app funciona en ZAVALA');
+  res.send('Hola, tu app funciona en ZAVALA - versión 2.0.0');
 });
 
 app.listen(PORT, () => {
